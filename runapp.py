1ea53c7e92defa3ceaac06876ccbcc7992c05a24
GiG <g.gargaro@gmail.com>
2025-01-29 12:17:38 +0100

    Update runapp.py

--- runapp.py
+++ runapp.py
@@ -1,6 +1,6 @@
 '''App Entrypoint For Gunicorn'''
 import os
-from app import create_app, db
+from app import create_app
 from debugger import initialize_debugger
 from logging import getLogger,basicConfig,DEBUG,getLogger,StreamHandler
 from sys import stdout
@@ -25,45 +25,5 @@ if __name__ == '__main__':
         )
         logger = getLogger('runapp.py')
         
-    if os.environ.get('REMOTE_DEBUG') == "true" or os.environ.get('ODO_DEBUG') == "true":
-        start_debug=True
-        debug_wait=True
-        try:
-            logger.info("Debugging Enabled")
-            debug_port=int(os.environ.get('DEBUG_PORT')) 
-            debug_host=os.environ.get('DEBUG_ADDRESS')
-            flask_port=os.environ.get('FLASK_PORT')
-            flask_host=os.environ.get('FLASK_HOST')
-            logger.info("Initializing Remote Debugger")
-            if os.environ.get('ODO_DEBUG') == "true":
-                debug_wait = False
-            initialize_debugger(debug_port=debug_port,debug_host=debug_host,debug_wait=debug_wait)
-        except Exception as e:
-            logger.error("Error in Remote Debugging: {}".format(e))
-            start_debug=False
     
-    if os.environ.get('REMOTE_DEBUG') == "true":        
-        if start_debug:
-            logger.info("Running Flask App with Remote Debugging")
-            try:
-                app.run(host=flask_host, port=flask_port,debug=True, use_debugger=False, use_reloader=False)
-            except OSError as error:
-                if "Address already in use" in str(error):
-                    logger.error("Port {} is already in use".format(flask_port))
-                else:
-                    logger.error("Error in Running Flask App: {}".format(error))
-    elif os.environ.get('ODO_DEBUG') == "true":
-        if start_debug:
-            logger.info("ODO Debugging Enabled")
-            flask_port=os.environ.get('FLASK_PORT')
-            flask_host=os.environ.get('FLASK_HOST')
-            try:
-                app.run(host=flask_host, port=flask_port,debug=True, use_debugger=False, use_reloader=False)
-            except OSError as error:
-                if "Address already in use" in str(error):
-                    logger.error("Port {} is already in use".format(flask_port))
-                else:
-                    logger.error("Error in Running Flask App: {}".format(error))
-    else:
-        logger.info("Debugging Disabled")
         app.run()
