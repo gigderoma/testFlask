52aa63c8e5f74f57b9b643a911f91773f7ad7e69
GiG <g.gargaro@gmail.com>
2025-01-29 12:19:39 +0100

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -9,7 +9,6 @@
 
 import sys
 from flask import Flask, request, current_app
-from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_mail import Mail
@@ -20,7 +19,6 @@ from config import Config
 from flask.logging import logging,default_handler
 
 
-db = SQLAlchemy()
 migrate = Migrate()
 bootstrap = Bootstrap()
 
@@ -31,7 +29,7 @@ def create_app(config_class=Config):
     # app.config['DEBUG'] = True
     app.config.from_object(config_class)
     #Check if we are running in Kubernetes by getting environment variables
-    if app.config['KUBERNETES_SERVICE_PORT'] != "" or app.debug:
+    if app.config['KUBERNETES_SERVICE_PORT'] != "" 
         #If Environment variable is not set we are not running in kubernetes
         #We will change the default handler to log to stdout
         app.logger.removeHandler(default_handler)
@@ -40,8 +38,6 @@ def create_app(config_class=Config):
     if app.config['GUNICORN_LOGGER'].handlers != []:
         app.logger.handlers = app.config['GUNICORN_LOGGER'].handlers
         app.logger.setLevel(app.config['GUNICORN_LOGGER'].level)
-    db.init_app(app)
-    migrate.init_app(app,db)
     bootstrap.init_app(app)
     from app.main import bp as main_bp
     app.register_blueprint(main_bp)
